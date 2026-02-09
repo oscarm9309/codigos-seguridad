8822cb12a1b4f8fb890f9ee24ed865e49cd9b05d
Oscar Andres Melo Oliveros <oamelo@syc.com.co>
2026-02-09 17:55:43 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { UseState } from "./UseState";
+import { ClassState } from "./ClassState";
+
+function App() {
+  
+  return (
+    <>
+      <UseState/>
+      <ClassState/>
+    </>
+  )
+}
+
+export default App
---
+++ src/ClassState/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ClassState extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Eliminar ClassState</h2>
+                
+                <p>Por favor, escribe el código de seguridad para eliminar este componente.</p>
+
+                <input type="text" placeholder="Código de seguridad"/>
+                <button>Comprobar</button>
+            </div>
+        );
+    }
+}
+
+export { ClassState };
