7648a8f5ca6ca898c46a9a4d55aa5ace8c1acabf
Oscar Melo <oscarm9309@gmail.com>
2026-02-11 21:37:49 -0500

    Se trabaja el useEffect acompañado de varios estados con useState

--- src/UseState/index.tsx
+++ src/UseState/index.tsx
@@ -1,23 +1,55 @@
 import React from "react";
 
+const SECURITY_CODE = "paradigma";
+
 function UseState({ name }: { name: string }) {
-const [error, setError] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [value, setValue] = React.useState("");
+  const [success, setSuccess] = React.useState(false);
 
-    return (
-            <div>
-                <h2>Eliminar {name}</h2>
-                
-                <p>Por favor, escribe el código de seguridad para eliminar este componente.</p>
+  React.useEffect(() => {
+    console.log("Empezando el efecto");
+    if (loading) {
+      setTimeout(() => {
+        console.log("Haciendo la validación");
 
-                {error && <p>Error: Código incorrecto</p>}
+        if (value === SECURITY_CODE) {
+          setError(false);
+          setSuccess(true);
+        } else {
+          setError(true);
+          setSuccess(false);
+        }
 
-                <input type="text" placeholder="Código de seguridad"/>
-                <button
-                    onClick={() => setError(!error)}>
-                    Comprobar
-                </button>
-            </div>
-        );
+        setLoading(false);
+        console.log("Terminando la validación");
+      }, 300);
+    }
+    console.log("Terminando el efecto");
+  }, [loading, value]);
+
+  return (
+    <div>
+      <h2>Eliminar {name}</h2>
+
+      <p>
+        Por favor, escribe el código de seguridad para eliminar este componente.
+      </p>
+
+      {error && <p>Error: Código incorrecto</p>}
+      {loading && <p>Cargando...</p>}
+      {success && <p>El código es correcto</p>}
+
+      <input
+        type="text"
+        placeholder="Código de seguridad"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => setLoading(true)}>Comprobar</button>
+    </div>
+  );
 }
 
 export { UseState };
